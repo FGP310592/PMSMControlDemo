1463a180cc11abb0f2d1cb7445e74e64c1da2892
Federico <59148362+FGP310592@users.noreply.github.com>
2022-04-05 16:38:11 +0200

    Add files via upload

---
+++ 1_GenerateMap_IdqRef/GenerateMap_IdqRef_Main_v04.m
@@ -0,0 +1,18 @@
+clear all
+close all
+clc
+%% Parameters
+% Inputs
+p         = 4;
+Rd        = 360e-3;
+Rq        = 360e-3;
+Ld        = 200e-6;
+Lq        = 400e-6;
+lambdadPM = 6.395415186893e-03;
+nm_max    = 7000;
+I_max     = 7.1;
+V_max     = 24/sqrt(3);
+C_max     = 0.3;
+Pm_max    = 117.0478;
+%% Map generation
+run('GenerateMap_IdqRef_Func_v04.p');
